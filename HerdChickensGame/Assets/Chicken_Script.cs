48b480f52e668fbaa8b4cfb678841efb9600cba6
Sophie <soph.bohr22@gmail.com>
2019-04-17 00:28:43 -0500

    Cleaned up animations, made ramp thicker

--- HerdChickensGame/Assets/Chicken_Script.cs
+++ HerdChickensGame/Assets/Chicken_Script.cs
@@ -32,7 +32,7 @@ public class Chicken_Script : MonoBehaviour
         /*
          * keep track of the location of the chicken, ensure it never
          * goes out of bounds, if it gets too far play an animation where it
-         * returns closer to the start?
+         * jumps closer to the center
          * 
          * 
          * keep track of the time, if the clock exceeded a certain
@@ -41,42 +41,35 @@ public class Chicken_Script : MonoBehaviour
          * the screen and scare them back in, thus ending the game
          */
     }
-    
+
     /*
      * need to alter this method so that it's not that it gets triggered by the 
      * ramp but that it gets triggered by being on the correct "track" because of the
      * 2D nature of the movement of the player
-     */ 
+     */
     private void OnTriggerEnter(Collider other)
     {
-        //random number generator for int numbers 0 to 10 for the jump/enter (10 numbers)
+        //random number generator for int numbers 1 (inclusive) to 10 (inclusive) for the jump/enter (10 numbers)
+        int rand_enter_num = Random.Range(1, 10);
+
         //random number generator for float numbers for speed of walk (0.0 to 1.0f ??)
-        
+        //float rand_speed = Random.Range(0.0f, 2.0f);
+
         if (other.gameObject.name == "Ramp")
         {
-            /*
-            if(rand_enter_num < 3) { //30% of the time 
+            if(rand_enter_num < 3) { //30% of the time FIXME:
                 //chicken will jump off
-                anim["Walk_Then_Jump].speed = rand_speed;
+                //anim["Jump_Test"].speed = 0.5f;
                 anim.Play("Walk_Then_Jump");
             } else { //70% of the time
                 //chicken will enter henhouse
-                anim["Walk_Up_Ramp].speed = rand_speed;
-                anim.Play("Walk_Up_Ramp");
-                Destroy(this.gameObject, 0.75f); //have to alter destroy time based on rand_speed
+                //anim["Walk_Up_Ramp].speed = rand_speed;
+                anim.Play("Walk_Into_Henhouse");
+                Destroy(this.gameObject, 1.7f); //have to alter destroy time based on rand_speed
+                
                 //play an animation of the girl jumping up and down once
-                to signal she's happy
+                //to signal she's happy
             }
-            */
-
-            anim["Walk_Up_Ramp"].speed = 1f;
-            anim.Play("Walk_Up_Ramp");
-
-            /*
-             * destroys the chicken after 0.75 seconds, which is approx. the length of the animation,
-             * this creates the illusion that the chicken enters the henhouse
-             */
-            Destroy(this.gameObject, 0.75f);
         }
     }
 }
